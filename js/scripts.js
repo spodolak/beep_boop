28aabc7d658123978feab1574c47034f89d5b718
Student <example@example.com>
2020-01-24 12:29:00 -0800

    refactor count function

--- js/scripts.js
+++ js/scripts.js
@@ -2,47 +2,36 @@ var replaceOneTwoThree = function (number) {
 	var numberArray = number.split("");
 	for (var index = 0; index <= (numberArray.length-1) ; index += 1) {
 		if (numberArray[index] === "3") {
-			return "3";
+			return "'I'm sorry, Dave. I'm afraid I can't do that.'";
 		} else if (numberArray[index] === "2") {
-			numberArray.forEach(function(digit)	{
-				if (digit === "3") {
-					console.log("I found a 3");
-					return "3";
-				} else {
-					console.log("I found a 2");
-					return "2";
-				}
-			});
 		} else if (numberArray[index] === "1") {
 		} else {
 		}
 		};
 	};
 
-	var findThree = function(number) {
-		var numberArray = number.split("");
-		console.log(numberArray);
-		numberArray.forEach(function(digit)	{
-			if (digit = "3") {
-				console.log("This string has a 3");
-			} else {
-				console.log("this is NOT a 3");
-			}
-		})
-	}
 
 
+	// var findThree = function(number) {
+	// 	var numberArray = number.split("");
+	// 	console.log(numberArray);
+	// 	numberArray.forEach(function(digit)	{
+	// 		if (digit = "3") {
+	// 			console.log("This string has a 3");
+	// 		} else {
+	// 			console.log("this is NOT a 3");
+	// 		}
+	// 	})
+	// }
 
+
+var replacement = "words?!*!"
 
 var count = function(number) {
 	var fullCount = "0";
 	for ( var index = 1; index <= number; index +=1)	{
-		if (index === 1) {
-			fullCount = (fullCount + ", " + "'Beep!'");
-		} else if (index === 2) {
-			fullCount = (fullCount + ", " + "'Boop!'");
-		} else if (index === 3) {
-			fullCount = (fullCount + ", " + "'I'm sorry, Dave. I'm afraid I can't do that.'");
+		if (index === 1 | index === 2 | index === 3) {
+			fullCount = (fullCount + ", " + replacement);
 		} else {
 			fullCount = (fullCount + ", " + index);
 		}
@@ -56,9 +45,11 @@ $(document).ready(function()	{
 		event.preventDefault();
 
 		var userNumber = $("input#numberIn").val();
-		findThree(userNumber);
+		var result = replaceOneTwoThree(userNumber);
 		var beepBoopCount = count(userNumber);
 		$("#beepBoopCount").text(beepBoopCount);
+		
+		alert(result);
 		
 
 	});
