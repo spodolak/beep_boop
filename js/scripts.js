d5bcd08d03c89e2f34b0ea9543afdf237a44eab8
Student <example@example.com>
2020-01-24 12:37:50 -0800

    link count function with function built to replace 1 2 and 3 digits. Debugging of replaceing function continues

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
+var replacementWord;
 var replaceOneTwoThree = function (number) {
 	var numberArray = number.split("");
 	for (var index = 0; index <= (numberArray.length-1) ; index += 1) {
 		if (numberArray[index] === "3") {
-			return "'I'm sorry, Dave. I'm afraid I can't do that.'";
+			return replacementWord = "'I'm sorry, Dave. I'm afraid I can't do that.'";
 		} else if (numberArray[index] === "2") {
+			return replacementWord = "'Boop!'";
 		} else if (numberArray[index] === "1") {
+			return replacementWord = "'Beep!'";
 		} else {
 		}
 		};
@@ -25,13 +28,14 @@ var replaceOneTwoThree = function (number) {
 	// }
 
 
+
 var replacement = "words?!*!"
 
 var count = function(number) {
 	var fullCount = "0";
 	for ( var index = 1; index <= number; index +=1)	{
 		if (index === 1 | index === 2 | index === 3) {
-			fullCount = (fullCount + ", " + replacement);
+			fullCount = (fullCount + ", " + replacementWord);
 		} else {
 			fullCount = (fullCount + ", " + index);
 		}
@@ -45,11 +49,10 @@ $(document).ready(function()	{
 		event.preventDefault();
 
 		var userNumber = $("input#numberIn").val();
-		var result = replaceOneTwoThree(userNumber);
+		replaceOneTwoThree(userNumber);
 		var beepBoopCount = count(userNumber);
 		$("#beepBoopCount").text(beepBoopCount);
 		
-		alert(result);
 		
 
 	});
