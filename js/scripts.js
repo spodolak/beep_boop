7d7bad4a8aa7ea5b66d8d216846f6ed298e34c53
Student <example@example.com>
2020-01-24 15:23:50 -0800

    debugg function replaceOneTwoThree. function replaceOneTwoThree does not prioritize digits 3 over 2 over 1 at this time.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,51 @@
-// var replacementWord;
-// var replaceOneTwoThree = function (number) {
-// 	var numberArray = number.split("");
-// 	for (var index = 0; index <= (numberArray.length-1) ; index += 1) {
-// 		if (numberArray[index] === "3") {
-// 			return replacementWord = "'I'm sorry, Dave. I'm afraid I can't do that.'";
-// 		} else if (numberArray[index] === "2") {
-// 			return replacementWord = "'Boop!'";
-// 		} else if (numberArray[index] === "1") {
-// 			return replacementWord = "'Beep!'";
-// 		} else {
-// 		}
-// 		};
-// 	};
-
+//Business Logic
+var replacementWord = ""; 
+var replaceOneTwoThree = function (number) {
+	var numberString = number.toString();
+	var numberArray = numberString.split("");
+	for (var index = 0; index <= (numberArray.length-1) ; index += 1) {
+		console.log(numberArray[index]);
+		if (numberArray[index] === "3") {
+			return "'I'm sorry, Dave. I'm afraid I can't do that.'";
+		} else if (numberArray[index] === "2") {
+			return replacementWord = "'Boop!'";
+		} else if (numberArray[index] === "1") {
+			return replacementWord = "'Beep!'";
+		} else {
+		}
+	};
+};
+console.log(replacementWord);
 var findOneTwoThree = function(number) {
 	var numberString = number.toString();
 	var numberArray = numberString.split(""); 
 	for (var index = 0; index <= (numberArray.length-1) ; index += 1)	{
 		if (numberArray[index] === "3" | numberArray[index] === "2" | numberArray[index] === "1"){
-			// alert(true);
 			return true;
 		} else {
-			// alert(false);
 		}
 	};
 };
 
-var replacementWord = "*HUKI%!";
 var count = function(number) {
 	var fullCount = "0";
 	for ( var index = 1; index <= number; index +=1)	{
 		if (findOneTwoThree(index) === true) {
+			replacementWord = replaceOneTwoThree(index);
 			fullCount = (fullCount + ", " + replacementWord)
 		} else {
 			fullCount = (fullCount + ", " + index);
 		}		
 	};
-	console.log(findOneTwoThree(index));
 	return fullCount;
 };
 
+//User Interface Logic
 
 $(document).ready(function()	{
 	$("form#beep-boop-form").submit(function()	{
 		event.preventDefault();
-
 		var userNumber = parseInt($("input#numberIn").val());
-
 		var beepBoopCount = count(userNumber);
 		$("#beepBoopCount").text(beepBoopCount);
 	});
