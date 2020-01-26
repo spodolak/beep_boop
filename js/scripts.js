4a83dcd46c34722b1e1b34a694079e28df51bcbe
Stephanie Podolak <StephPodolak@gmail.com>
2020-01-26 13:15:44 -0800

    add gh-pages link to README.md, refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,7 @@
 
 //Function takes numbers containing digits 3, 2, or 1 in priority order and replaces them with phrases.
 var replaceOneTwoThree = function (number) {
-	var numberString = number.toString();
-	var numberArray = numberString.split("");
+	var numberArray = number.toString().split("");
 	for (var index = 0; index <= (numberArray.length-1) ; index += 1) {
 		if (numberArray[index] === "3") {
 			return "'I'm sorry, Dave. I'm afraid I can't do that.'";
@@ -18,27 +17,41 @@ var replaceOneTwoThree = function (number) {
 
 //Function searches each number of the count to see if it contains digits 1, 2, or 3.
 var findOneTwoThree = function(number) {
-	var numberString = number.toString();
-	var numberArray = numberString.split(""); 
+	var numberArray = number.toString().split(""); 
 	for (var index = 0; index <= (numberArray.length-1) ; index += 1)	{
-		if (numberArray[index] === "3" | numberArray[index] === "2" | numberArray[index] === "1"){
+		if (numberArray[index] === "3" | numberArray[index] === "2" | numberArray[index] === "1")	{
 			return true;
+		} else { 
+		}
+	};
+};
+
+var findNonNumbers = function(number)	{
+	var numberArray = number.toString().split(""); 
+	for (var index = 0; index <= (numberArray.length-1) ; index += 1) {
+		if (isNaN(number) === true)	{
+			return true; 
 		} else {
 		}
 	};
 };
 
 //Function counts from zero up to the number value of the user input
-var count = function(number) {
+var countToNumber = function(number) {
 	var fullCount = "0";
-	for ( var index = 1; index <= number; index +=1)	{
-		if (findOneTwoThree(index) === true) {
-			replacementWord = replaceOneTwoThree(index);
-			fullCount = (fullCount + ", " + replacementWord)
-		} else {
-			fullCount = (fullCount + ", " + index);
-		}		
-	};
+	if (findNonNumbers(number) === true)	{
+		alert("Hey! That's not a number!");
+		return
+	} else {
+		for ( var index = 1; index <= number; index +=1) 	{
+			if (findOneTwoThree(index) === true) {
+				replacementWord = replaceOneTwoThree(index);
+				fullCount = (fullCount + ", " + replacementWord)
+			} else {
+				fullCount = (fullCount + ", " + index);
+			}		
+		};
+	}
 	return fullCount;
 };
 
@@ -47,8 +60,6 @@ var count = function(number) {
 $(document).ready(function()	{
 	$("form#beep-boop-form").submit(function()	{
 		event.preventDefault();
-		var userNumber = parseInt($("input#numberIn").val());
-		var beepBoopCount = count(userNumber);
-		$("#beepBoopCount").text(beepBoopCount);
+		$("#beepBoopCount").text(countToNumber(parseInt($("input#numberIn").val())));
 	});
 });
